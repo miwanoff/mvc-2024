b6972fd7b94613e95f1b5bb5a87dadde3ead652e
miwanoff <marina.iva.noffa@gmail.com>
2024-02-03 15:20:29 +0200

    Initial commit

---
+++ view.php
@@ -0,0 +1,25 @@
+<!— MVC. Шаблон гостевой книги. —>
+    <html>
+
+    <head>
+        <title>Гостевая книга</title>
+    </head>
+
+    <body>
+        <h2>Добавьте свое сообщение:</h2>
+        <form action="controller.php" method="post">
+            Ваше имя: <input type=text name="new[name]"><br /> Комментарий:<br />
+            <textarea name="new[text]" cols="60" rows="5"></textarea>
+            <br /> <input type="submit" name="doAdd" value="Добавить!">
+        </form>
+        <h2>Гостевая книга:</h2>
+        <?php foreach ($Book as $id => $v) { ?>
+        Время: <?= date("d.m.Y H:i:s", $id) ?><br />
+        Имя: <?= $v['name'] ?><br />
+        Комментарий:<br />
+        <?= nl2br($v['text']) ?>
+        <hr />
+        <?php } ?>
+    </body>
+
+    </html>
